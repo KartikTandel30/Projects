76e6b1ad4ead3d9a170d8d440539fc043aa347ab
Kartik <Kartik-Suresh.Tandel@student.tu-freiberg.de>
2025-05-25 21:30:30 +0200

    after trying simple mesht

--- task_1.py
+++ task_1.py
@@ -4,9 +4,19 @@ import ufl
 from mpi4py import MPI
 from petsc4py import PETSc
 import pyvista as pv
-#import pyvistaqt as pvqt
 
+from basix.ufl import element, mixed_element
+from dolfinx import default_real_type, log, plot
+from dolfinx.fem import Function, functionspace
+from dolfinx.fem.petsc import NonlinearProblem
+from dolfinx.io import XDMFFile
+from dolfinx.mesh import CellType, create_unit_square
+from dolfinx.nls.petsc import NewtonSolver
+from ufl import dx, grad, inner
 
+mesh=create_unit_square(MPI.COMM_WORLD, 96, 96, CellType.triangle)  #creating mesh basic
+P1 = element("Lagrange", mesh.basix_cell(), 1, dtype=default_real_type) #slecting the elemnt type
+ME = functionspace(mesh, mixed_element([P1, P1])) #
 
 
 
