d3b5d7ffe4cb2e5bbd31faa97c305f9c4841b95b
Kartik <Kartik-Suresh.Tandel@student.tu-freiberg.de>
2025-05-31 17:54:56 +0200

    after mesh

--- task_1.py
+++ task_1.py
@@ -1,10 +1,8 @@
-import numpy as np
-import dolfinx
-import ufl
-from mpi4py import MPI
 from petsc4py import PETSc
-import pyvista as pv
+import dolfinx
+from mpi4py import MPI
 
+import ufl
 from basix.ufl import element, mixed_element
 from dolfinx import default_real_type, log, plot
 from dolfinx.fem import Function, functionspace
@@ -13,10 +11,18 @@ from dolfinx.io import XDMFFile
 from dolfinx.mesh import CellType, create_unit_square
 from dolfinx.nls.petsc import NewtonSolver
 from ufl import dx, grad, inner
+import pyvista as pv
 
-mesh=create_unit_square(MPI.COMM_WORLD, 96, 96, CellType.triangle)  #creating mesh basic
-P1 = element("Lagrange", mesh.basix_cell(), 1, dtype=default_real_type) #slecting the elemnt type
-ME = functionspace(mesh, mixed_element([P1, P1])) #
+
+# Step 1: Create mesh
+msh = create_unit_square(MPI.COMM_WORLD, 96, 96, CellType.triangle)
+P1 = element("Lagrange", msh.basix_cell(), 1, dtype=default_real_type)
+ME = functionspace(msh,P1)
+
+
+
+
+
 
 
 
