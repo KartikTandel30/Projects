111c3d9072b9217132f7094f2b848ab1cb385820
Kartik <Kartik-Suresh.Tandel@student.tu-freiberg.de>
2025-06-02 23:27:40 +0200

    mesh viewing

--- task_1.py
+++ task_1.py
@@ -19,6 +19,36 @@ msh = create_unit_square(MPI.COMM_WORLD, 96, 96, CellType.triangle)
 P1 = element("Lagrange", msh.basix_cell(), 1, dtype=default_real_type)
 ME = functionspace(msh,P1)
 
+w_phi = ufl.TestFunctions(ME) # test function for order parameter
+
+phi = Function(ME) # trial function n+1
+
+phi_0 = Function(ME) # previous value
+
+# Boundary condition application
+
+
+
+
+''' to view the mesh created '''
+# outputing the mesh file to view in other source( paraview )
+file = XDMFFile(MPI.COMM_WORLD, "demo_ch/output.xdmf", "w")
+file.write_mesh(msh)
+file.close()
+
+
+topology, cell_types, x = plot.vtk_mesh(msh,msh.topology.dim)
+
+#  Visualize using PyVista 
+if MPI.COMM_WORLD.rank == 0:
+    grid = pv.UnstructuredGrid(topology, cell_types, x)
+    plotter = pv.Plotter()
+    plotter.add_mesh(grid, show_edges=True, color="white")
+    plotter.view_xy()
+    plotter.add_text("Mesh grid", font_size=10)
+    plotter.show()
+
+
 
 
 
